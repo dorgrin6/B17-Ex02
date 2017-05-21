976331e8ba698760d8d309c83279cefd860bf224
Dor Grinshpan <dorgrin6@gmail.com>
2017-05-21 12:20:19 +0300

    Add project files.

---
+++ B17 Ex02 Dor 201389681 Idan 307948836/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B17_Ex02_Dor_201389681_Idan_307948836
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
